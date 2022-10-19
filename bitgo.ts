417ea7fcee4814e0ee806335b5f20a7af976f0ad
Alpay Aldemir <alpayaldemir@bitgo.com>
2022-10-19 12:41:05 -0400

    point to testnet-10

--- bitgo.ts
+++ bitgo.ts
@@ -10,28 +10,29 @@ export enum Environment {
 
 const rootURI = {
   test: 'https://app.bitgo-test.com',
+  custom: 'https://testnet-10-app.bitgo-dev.com',
 }
 
 export enum WalletVersion {
   TSS = 3,
 }
 
-export const environment =  Environment.TEST;
+export const environment =  Environment.CUSTOM;
 
 export const bitgo = new BitGo({
-  accessToken: accessToken[Environment.TEST],
-  env: Environment.TEST,
-  customRootURI: rootURI.test,
+  accessToken: accessToken[environment],
+  env: environment,
+  customRootURI: rootURI.custom,
 });
 
 // TODO set enterprise ID here
 export const enterprise = {
-  [Environment.TEST]: ''
+  [Environment.CUSTOM]: '63502a2ede81e00007f33ebe5e39bb58'
 }
 
 export const walletVersion = WalletVersion.TSS;
 export const multisigType = 'tss';
-export const enterpriseId = enterprise[Environment.TEST];
+export const enterpriseId = enterprise[environment];
 
 // TODO set walletId and baseAddress here after wallet generation
 export const walletId = '';
